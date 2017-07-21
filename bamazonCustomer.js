cc1d879178e0e730f461b3a35c56990a6e75b4cc
cxwang001 <william@Williams-MBP.attlocal.net>
2017-07-20 18:30:59 -0700

    Fixing minor bugs in DB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,8 +1,8 @@
+
 var mysql = require("mysql");
 var inquirer = require("inquirer");
-var Table = require('cli-table');
 require('console.table');
-
+//making mysql DB connection
 var connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -13,48 +13,27 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
-// connection.connect(function(err){
-//   if(err) throw err;
-//   printTable();
-//   //  
-// });
+console.log("\nWe have the following products in stock. Please make purchase!")
 //Print overall table
 function printTable(){
 	connection.connect(function(err) {
-  		// if (err) throw err;
-  		// console.log("connected as id " + connection.threadId);
   		connection.query("SELECT * FROM products", function(err, res) {
     	if (err) throw err;
-         	console.table(res);
-          askQuestion();
+         	console.table(res); //Print out table using console.table
+          askQuestion(); //call the next function
   		});
-  		// connection.end();
-		
 	});
-
 };
-printTable();
-// function sequence() {
-//  askQuestion(function() {
-//   printTable();
-//  })
-// };
-
-// printTable();
-
-// askQuestion();
-
+printTable(); //call inital products table functtion
 
 function askQuestion() {
-
-   
-  // prompt for info about the item being put up for auction
+// prompt for info about the item being put up for purchase
   	inquirer
     .prompt([
       {
         name: "item",
         type: "input",
-        message: "What is the ID of the product you would like to buy",
+        message: "What is the ID of the product you would like to buy?",
         validate: function(value) {
           if (isNaN(value) === false && value < 11){
             return true;
@@ -73,54 +52,38 @@ function askQuestion() {
           return false;
         }
       }
-     ])
-      .then(function(answer) {
-        // var uQuan = answer.user_quantity;
-        // var uItem = answer.item;
-      
-        connection.query("SELECT * FROM products WHERE ?", 
+     ]).then(function(answer) { //Query out the product is being purchased
+          connection.query("SELECT * FROM products WHERE ?", 
             [{
               item_id: answer.item
             }], 
             function(err, res){
               if (err) throw err;
-              console.table(res);
+                // console.table(res);
                 var current_quality = res[0].stock_quantity;
-                console.log(current_quality);
+                console.log("\nWe have " + current_quality + " units in stock right now.");
                 var price = res[0].price;
                 var left_quality = current_quality - answer.user_quantity
                 if (current_quality > answer.user_quantity) {
-                 
-                  
-                  console.log("Ammount left: " + left_quality);
-                  console.log("Cost of purchase = " +(answer.user_quantity * price));
-                  //then update DB
-                  connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", 
+                    console.log("\nTotay Cost of this purchase will be $ " +(answer.user_quantity * price));
+                    //then update DB the quatity where the product ID has been selected
+                    connection.query("UPDATE products SET stock_quantity=? WHERE item_id=?", 
                     [left_quality, answer.item], 
-
-
-
                     function(err, res) {
-                    console.table(res);
-                    
-                  });
-
-                  connection.query("SELECT * FROM products", function (err, res) {
+                      if (err) throw err;
+                    });
+                  //Showing the updated table
+                    connection.query("SELECT * FROM products", function (err, res) {
                     console.table(res)
-                  });
+                    });
               
-          } else{
-            console.log("You don't have enought!!")
-          }
-
-// };
-
-
+                } else{ //if not enough in stock, no purchase!!
+                  console.log("Sorry, we don't have enough quatity in stock!")
+                };
               connection.end();
             });
 
-        });
-      // printTable();
-};  
+      });
+};  //end of askQuestion function
 
 
